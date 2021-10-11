04d2675f64606d6fada3274de0c2b5646c6dfc44
Paul Applegate <paul@theapplegates.com>
2021-10-11 18:52:45 -0400

    first commit

---
+++ gatsby-browser.js
@@ -0,0 +1,21 @@
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
+//config.autoAddCss = false
+
+// custom typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
+
+//config.autoAddCss = false
+
+// const styles = require("@fortawesome/fontawesome-svg-core/styles.css")
+// const config = require("@fortawesome/fontawesome-svg-core")
+
+
